2a029452adb914411d1a84e721a0265b701aa6d5
gn6me <mrt.sleeper@gmail.com>
2025-02-21 03:59:56 -0500

    update

---
+++ alpineinit.js
@@ -0,0 +1,15 @@
+document.addEventListener('alpine:init', () => {
+    Alpine.data('searchlist', () => ({
+        search: '',
+        open: false,
+        toggle() {
+            this.open = ! this.open
+        }
+        students: ['shiro', 'saya', 'aru'],
+        get filteredItems() {
+            return this.items.filter(
+                i => i.startsWith(this.search)
+            )
+        }
+    }))
+})
