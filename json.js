a38defd010075bea66d640d7bcb845328b857c59
gn6me <mrt.sleeper@gmail.com>
2025-02-21 02:45:36 -0500

    update

--- json.js
+++ json.js
@@ -1,7 +1,40 @@
-var students = $.getJSON("student-list.json", function () {
-  console.log("success");
-});
+const form = document.querySelector(".form");
+const input = form.querySelector("input");
+const searchResults = document.querySelector(".search-results");
+const searchIcon = document.querySelector(".searchIcon");
 
-students.always(function () {
-  console.log("second complete");
+const searchStudents = async (query) => {
+  const jsonFile = "student-list.json";
+};
+
+try {
+  const response = await fetch(jsonFile);
+  const data = await response.json();
+  const students = data.results;
+
+  searchResults.innerHTML = "";
+  students.forEach((student) => {
+    const { name, school, role } = student;
+
+    const studentContainer = document.createElement("div");
+    studentContainer.classList.add("student");
+
+    studentContainer.innerHTML = `
+          <div class="student-info">
+              <h3>${name}</h3>
+              <small>Released on ${school}</small>
+              <span class="rating">${role}</span>
+          </div>
+        `;
+
+    searchResults.appendChild(studentContainer);
+  });
+} catch (error) {
+  console.error(error);
+}
+
+form.addEventListener("input", async (event) => {
+  event.preventDefault();
+  const query = input.value;
+  searchStudents(query);
 });
