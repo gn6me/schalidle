4f8357b9e71eb491e78dda47f194480ce331b8da
gn6me <mrt.sleeper@gmail.com>
2025-02-21 01:37:54 -0500

    update

--- json.js
+++ json.js
@@ -1,5 +1,7 @@
-var request = new HttpRequest();
-request.open("GET", "student-list.json", false);
-request.send(null);
-var studentList = JSON.parse(request.responseText);
-console.log(studentList);
+var students = $.getJSON("student-list.json", function () {
+  console.log("success");
+});
+
+students.always(function () {
+  console.log("second complete");
+});
