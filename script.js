d3ce32735a46c4f8cb491bc1bae4c20c2893841d
gn6me <mrt.sleeper@gmail.com>
2025-02-23 03:11:03 -0500

    mistake

--- script.js
+++ script.js
@@ -189,7 +189,7 @@ function displayGuess(character) {
             img.style.height = '100%';
             img.style.objectFit = 'cover';
             square.appendChild(img);
-        } else if (attr === character.damageType || attr === character.armorType || attr === character.role || attr === character.combatClass) {
+        } else if (attr === character.damageType || attr === character.armorType || attr === character.role) {
             const dmgImg = document.createElement('img');
             dmgImg.src ='https://schalidle.vercel.app/imgs/info/' + attr + '.webp';
             dmgImg.className = 'dmgIcon';
@@ -199,7 +199,7 @@ function displayGuess(character) {
             schoolImg.src = 'https://schalidle.vercel.app/imgs/schools/' + attr + '_Icon.webp';
             schoolImg.className = 'schoolImg';
             square.appendChild(schoolImg);
-        } else if (attr === character.role) {
+        } else if (attr === character.combatClass) {
             const roleImg = document.createElement('img');
             roleImg.src ='https://schalidle.vercel.app/imgs/info/' + attr + '_role.webp';
             roleImg.className = 'dmgIcon';
