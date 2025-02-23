792a5a24b8403c1ac47ab81336b31dfb08464767
gn6me <mrt.sleeper@gmail.com>
2025-02-23 02:41:23 -0500

    testing

--- script.js
+++ script.js
@@ -189,7 +189,7 @@ function displayGuess(character) {
             img.style.height = '100%';
             img.style.objectFit = 'cover';
             square.appendChild(img);
-        } else if (attr === 'Explosion' || attr === 'Mystic') {
+        } else if (attr === 'Explosion' || attr === 'Mystic'|| attr === 'Piercing' || attr === 'Sonic' || attr === 'Heavy' || attr === 'Light' || attr === 'Special' || attr === 'Elastic') {
             const dmgImg = document.createElement('img');
             dmgImg.src ='https://schalidle.vercel.app/imgs/info/' + attr + '.webp';
             dmgImg.className = 'dmgIcon';
